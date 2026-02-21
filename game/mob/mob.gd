fe179f69962b549661cc4d015e7ea6907c56b109
Jack Darker <JackDarker@freenet.de>
2026-02-21 10:32:49 +0100

    finally got that rotation working

--- game/mob/mob.gd
+++ game/mob/mob.gd
@@ -3,11 +3,15 @@ extends CharacterBody3D
 #Note: z- has to be front of mob-skin 
 # 
 
-enum STATE {DEAD=0,IDLE=1,FOLLOW=2,SEARCH=3,FLEE=4,ATTACK=5,GOHOME=6}
+enum STATE {DEAD=0,IDLE=1,FOLLOW=2,SEARCH=3,FLEE=4,ATTACK=5,GOHOME=6,PATROL=7}
 
 @export var speed:float = 3
-@export var rotation_speed:float = 1
+@export var rotation_speed:float = 3
 @export var home_pos:Vector3 = Vector3.ZERO
+## a node with PatrolPoint-children
+@export var patrol_path:Node
+var curr_patrol_point:PatrolPoint
+var last_patrol_point:PatrolPoint
 var _player_pos:Vector3
 var _target_pos:Vector3	
 var _gravity := -30.0
@@ -49,7 +53,7 @@ func _process(delta: float) -> void:
 			%nav_agent.target_position=_player_pos
 			#_state=STATE.ATTACK
 	#follow if in view		
-	if(_state==STATE.IDLE || _state==STATE.FOLLOW):
+	if(_state==STATE.IDLE || _state==STATE.FOLLOW || _state==STATE.PATROL):
 		colliders=%view_range.collision_result
 		for collider in colliders:
 			if collider.collider.is_in_group("Player"):
@@ -61,30 +65,64 @@ func _process(delta: float) -> void:
 		if(%nav_agent.is_navigation_finished()):
 			_state=STATE.SEARCH
 	
-	if(_state==STATE.SEARCH):	#search around point last spotted
+	if(_state==STATE.SEARCH):	#search around point last spotted	#TODO search in direction where the player left the view cone
 		if(_prev_state!=STATE.SEARCH):
 			%state_timer.wait_time=5
 			%state_timer.start()
 			%nav_agent.target_position=_player_pos+Vector3(0,0,5).rotated(Vector3.UP,randf()*PI)
 		elif(%nav_agent.is_navigation_finished()):
 			%nav_agent.target_position=_player_pos+Vector3(0,0,8).rotated(Vector3.UP,randf()*PI)
+	
+	if(_state==STATE.IDLE && patrol_path):	#patrolling
+		continue_patrol()
+			
+	if(_state==STATE.PATROL):
+		if(%nav_agent.is_navigation_finished()):
+			last_patrol_point=curr_patrol_point
+			curr_patrol_point=null
+			continue_patrol()
 
+func continue_patrol():
+	var points=patrol_path.get_children()
+	for i in points.size():
+		if(!curr_patrol_point && !last_patrol_point):	#start patrol on first node #TODO closest node?
+			curr_patrol_point=points[0]
+			break
+		if(curr_patrol_point):	#continue
+			break
+		if(last_patrol_point && points[i-1]==last_patrol_point):# find next
+			curr_patrol_point=points[i]
+			break
+		if(i==points.size()-1):	#start from begin
+			curr_patrol_point=points[0]
+			break
+			
+	if(curr_patrol_point):
+		_state=STATE.PATROL		
+		%nav_agent.target_position=curr_patrol_point.global_position
 
 func _physics_process(delta):
 	var _next:Vector3
 	#var direction = (player.global_position - global_position).normalized()
-	if(_state==STATE.FOLLOW || _state==STATE.SEARCH):
-		#if(!%nav_agent.is_navigation_finished() && !%nav_agent.is_target_reached()):
-			_next=%nav_agent.target_position
-			_next.y=global_position.y
-			_target_pos.y=global_position.y
-			_target_pos=_target_pos.lerp(_next,rotation_speed*delta)#.slide(Vector3.UP)#.project(Vector3.UP)
-			look_at(_target_pos)
-			_target_pos.y=global_position.y
-			var direction = (_target_pos-global_position).normalized()
-			velocity = direction * speed
-			var y_velocity := velocity.y
-			velocity.y =  y_velocity+_gravity * delta
+	if(_state==STATE.FOLLOW || _state==STATE.SEARCH || _state==STATE.PATROL):
+		#_next=%nav_agent.get_next_path_position()# .target_position
+		#_next.y=global_position.y
+		#_target_pos.y=global_position.y
+		#_target_pos=_target_pos.lerp(_next,rotation_speed*delta)#for reasons i dont understand this will make the char rotate oddly close to endpoint of path
+		#look_at(_target_pos)
+		#_target_pos.y=global_position.y
+		#var direction = (_target_pos-global_position).normalized()
+		#velocity = direction * speed
+		#var y_velocity := velocity.y
+		#velocity.y =  y_velocity+_gravity * delta
+		_next=%nav_agent.get_next_path_position()
+		var direction = (_next-global_position).normalized()
+		velocity = direction * speed
+		var y_velocity := velocity.y
+		velocity.y =  y_velocity+_gravity * delta
+		if(_next!=Vector3.ZERO && direction.length()>0):
+			var target_rotation = direction.signed_angle_to(Vector3.FORWARD,Vector3.DOWN)
+			rotation.y = lerp_angle(rotation.y, target_rotation, rotation_speed * delta)
 	else:
 		velocity=Vector3.ZERO
 	move_and_slide()
---
+++ game/patrol_point.gd
@@ -0,0 +1,1 @@
+class_name PatrolPoint extends Node3D
